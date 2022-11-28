20921affe8856ad26fa988930843220c205fe087
Alexander Masztakowski <alexander.masztakowski@gmail.com>
2022-11-28 09:58:34 +0100

    modifications to files

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@
 mod key_listener;
 mod map_generator;
 fn main() {
-    map_generator::generator();
+    
     // print!("{}", termion::cursor::Hide);
     // key_listener::keylistener();
     // print!("{}", termion::cursor::Show);
-}
+}   
--- src/map_generator.rs
+++ src/map_generator.rs
@@ -9,7 +9,7 @@ impl Default for MazeTile {
     fn default() -> MazeTile {
         MazeTile {
             visited: false,
-            possibilities: [true, true, true, true, true, true, true, true, true, true, true]
+            possibilities: [true; 11]
         }
     }
 }
@@ -22,14 +22,11 @@ impl Clone for MazeTile {
         }
     }
 }
+
 pub fn generator() {
     let def = MazeTile {..Default::default()};
     let mut grid: [[MazeTile; 100]; 100] = [[def; 100]; 100];
 
-    let mut x: u32 = 1;
-    for element in grid.iter_mut().flat_map(|r| r.iter_mut()) {
-        println!("{}, {}", x, element.visited);
-        x = x+1;
-    }
+    
     
 }
